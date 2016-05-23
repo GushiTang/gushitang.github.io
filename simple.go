c52d5f5527a3c888f4373bee026b4d108ece1679
GushiTang <gusgitang@gmail.com>
2016-05-23 17:44:30 -0400

    Test server, html and json

---
+++ simple.go
@@ -0,0 +1,22 @@
+// dev web server
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+const (
+    // INDEX = "index.html"
+    INDEX = "kapu.html"
+)
+
+func main() {
+    fmt.Println("starting web on http://127.0.0.1:8000")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println(r)
+        http.ServeFile(w, r, INDEX)
+    })
+    log.Fatal(http.ListenAndServe(":8000", nil))
+}
